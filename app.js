5ed88dc8fd812b32764706cc6fd00eb94ea8a108
Juan Mosquella <jmosquella11@gmail.com>
2022-06-12 19:05:57 -0300

    Web finalizada

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
+const questions = document.querySelectorAll(".container-question");
+const question = document.querySelectorAll('.fa-angle-down')
+
 const menuBtn = document.getElementById('menu-btn');
     const nav = document.querySelector('.nav');
 
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('active')
+})
+
+
+document.addEventListener('click', (e) => {
+    const containerQuestion = e.target.parentElement.parentElement;  
+    questions.forEach(el => {
+        console.log(el)
+        el.classList.remove('active')
+    }) 
+
+    if(e.target.matches('.fa-angle-down')){
+        containerQuestion.classList.toggle('active')       
+    }
+
+    
+
+    
 })
